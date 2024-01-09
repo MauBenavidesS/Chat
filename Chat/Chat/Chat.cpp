39356985dc9aa9907eb95281fd7a8571ad1fd772
Mauricio Benavides <mauricio_benavides_za@mail.toyota.co.jp>
2024-01-09 15:10:11 +0900

    [Not Compiling] V0.1

--- Chat/Chat/Chat.cpp
+++ Chat/Chat/Chat.cpp
@@ -1,12 +1,41 @@
-﻿// Chat.cpp : Defines the entry point for the application.
-//
+﻿#include "UseImGui.h"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
-#include <iostream>
-
-using namespace std;
-
-int main()
+int main(void) 
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	// Setup window
+	if (!glfwInit())
+		return 1;
+
+	// GL 3.0 + GLSL 130
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+	// Create window with graphics context
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui - Chat", NULL, NULL);
+	if (window == NULL)
+		return 1;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) // tie window context to glad's ope...
+		throw("Unable to context to OpenGL");
+
+	int screen_width, screen_height;
+	glfwGetFramebufferSize(window, &screen_width, &screen_height);
+	glViewport(0, 0, screen_width, screen_height);
+
+	UseImGui myimgui;
+	myimgui.Init(window, glsl_version);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		myimgui.Update();
+		myimgui.Render();
+		glfwSwapBuffers(window);
+	}
+	myimgui.Shutdown();
 }
---
+++ Chat/Chat/UseImGui.cpp
@@ -0,0 +1,47 @@
+#include "UseImGui.h"
+
+bool show_another_window = true;
+
+void UseImGui::Init(GLFWwindow* window, const char* glsl_version)
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+	ImGui::StyleColorsDark();
+}
+
+void UseImGui::NewFrame()
+{
+	// feed inputs to dear imgui, start new frame
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+}
+
+void UseImGui::Update()
+{
+	ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+	ImGui::Text("Hello from another window!");
+	if (ImGui::Button("Close Me"))
+		show_another_window = false;
+	ImGui::End();
+}
+
+void UseImGui::Render()
+{
+	// Render dear imgui into screen
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void UseImGui::Shutdown()
+{
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+}
