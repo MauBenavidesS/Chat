b2d2f556afdc3da9f3e9b45fed34419da0cd4d33
Mauricio Benavides <mauricio_benavides_za@mail.toyota.co.jp>
2024-03-13 10:07:25 +0900

    [Fix] Cursor in InputText box

--- Chat/Chat/ChatApp.cpp
+++ Chat/Chat/ChatApp.cpp
@@ -11,6 +11,9 @@ int messageCount = 0;
 std::vector<std::string> messages;
 char inputBuffer[MAX_INPUT_LENGTH] = "";
 
+// Buttons
+static bool send = false;
+
 void ChatApp(void) {
 	ImGui::Begin("Chat++", NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
 
@@ -24,21 +27,22 @@ void ChatApp(void) {
 
 	ImGui::Separator();
 
-	// Check if the mouse is hovering over the specific child window
-	bool hoveringOverChatLog = ImGui::IsWindowHovered(ImGuiHoveredFlags_ChildWindows);
+	////// Check if the mouse is hovering over the specific child window
+	//bool hoveringOverChatLog = ImGui::IsWindowHovered(ImGuiHoveredFlags_ChildWindows);
 	bool isMouseLeftPressed = ImGui::IsMouseDown(ImGuiMouseButton_Left);
+	bool isChatWindowFocused = ImGui::IsWindowFocused();
 
-	if (((hoveringOverChatLog) && (isMouseLeftPressed)) || (ImGui::IsKeyPressed(ImGuiKey_Enter))) {
-		// Input box    
+	if (send || (ImGui::IsKeyPressed(ImGuiKey_Enter) && messageCount < MAX_MESSAGES&& strlen(inputBuffer) >= MIN_CHARACTER)) {
 		ImGui::SetKeyboardFocusHere();
-		ImGui::InputText("##Input", inputBuffer, IM_ARRAYSIZE(inputBuffer));
-		ImGui::SameLine();
 	}
 	ImGui::InputText("##Input", inputBuffer, IM_ARRAYSIZE(inputBuffer));
 	ImGui::SameLine();
+	send = ImGui::Button("Send");
+	if ((send || ImGui::IsKeyPressed(ImGuiKey_Enter)) && messageCount < MAX_MESSAGES && strlen(inputBuffer) >= MIN_CHARACTER) {
+		printf("1. Send pressed. \n");
+		ImGuiID buttonID = ImGui::GetID("Send");
+		printf("Button ID: %u\n", buttonID);
 
-	// Send 
-	if ((ImGui::Button("Send") || ImGui::IsKeyPressed(ImGuiKey_Enter)) && messageCount < MAX_MESSAGES && strlen(inputBuffer) >= MIN_CHARACTER) {
 		// Add the new message at the bottom
 		std::string message(inputBuffer);
 		messages.push_back(message);
@@ -49,21 +53,22 @@ void ChatApp(void) {
 		// Clear the input box
 		inputBuffer[0] = char(0);
 	}
-	// Drag
-	//// 1. Detect Drag // 2. Know text position.
-	bool isDragging0 = ImGui::IsMouseDragging(0, -1.0f);
-	bool isDragging1 = ImGui::IsMouseDragging(1, -1.0f);
-	bool isDragging2 = ImGui::IsMouseDragging(2, -1.0f);
 
-	if (isDragging0) {
-		printf("0. Mouse is dragging. \n");
-	}
-	else if (isDragging1) {
-		printf("1. Mouse is dragging. \n");
-	}
-	else if (isDragging2) {
-		printf("2. Mouse is dragging. \n");
-	}
+	//// Drag
+	////// 1. Detect Drag // 2. Know text position.
+	//bool isDragging0 = ImGui::IsMouseDragging(0, -1.0f);
+	//bool isDragging1 = ImGui::IsMouseDragging(1, -1.0f);
+	//bool isDragging2 = ImGui::IsMouseDragging(2, -1.0f);
+
+	//if (isDragging0) {
+	//	printf("0. Mouse is dragging. \n");
+	//}
+	//else if (isDragging1) {
+	//	printf("1. Mouse is dragging. \n");
+	//}
+	//else if (isDragging2) {
+	//	printf("2. Mouse is dragging. \n");
+	//}
 
 	ImGui::End();
 }
