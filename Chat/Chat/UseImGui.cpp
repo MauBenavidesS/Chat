cb619eeb7e2d48af74eb6aa42ce62cd855ed0085
Mauricio Benavides <mauricio_benavides_za@mail.toyota.co.jp>
2024-03-14 15:16:11 +0900

    [WebSocket++] Support Added

--- Chat/Chat/UseImGui.cpp
+++ Chat/Chat/UseImGui.cpp
@@ -1,4 +1,5 @@
 #include "UseImGui.h"
+#include "WebSocketSend.h"
 #include "ChatApp.h"
 #include <algorithm>
 
@@ -39,6 +40,7 @@ void UseImGui::Update()
 
 	// App's Entry Point
 	ChatApp();
+	WebSocketSend();
 
 	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
 	if (show_demo_window)
